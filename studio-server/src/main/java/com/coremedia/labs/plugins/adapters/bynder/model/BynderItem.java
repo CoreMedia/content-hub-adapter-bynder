70dcf7f555d2446f0ac68fc637b2d1a217211d8a
ydai <yiling.dai@coremedia.com>
2024-06-11 09:23:49 +0200

    2401.3 CoreMedia Compatibility and Maintenance Upgrade
    * Use Java 17 SDK
    * Use Node 18.x

--- studio-server/src/main/java/com/coremedia/labs/plugins/adapters/bynder/model/BynderItem.java
+++ studio-server/src/main/java/com/coremedia/labs/plugins/adapters/bynder/model/BynderItem.java
@@ -13,7 +13,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.activation.MimeType;
+import jakarta.activation.MimeType;
 import java.io.BufferedInputStream;
 import java.io.InputStream;
 import java.text.DateFormat;
